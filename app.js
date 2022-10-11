a27ecae4870b731923ee476f3d3c4f2750ee9cf0
gxrysu <garysu2013@gmail.com>
2022-10-11 12:31:12 +0100

    completes GET /api/articles with added comment_count for each article

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const { getTopics, getArticleById, getUsers, patchArticleById } = require("./controller/controller.js");
+const { getTopics, getArticleById, getUsers, getArticles,patchArticleById } = require("./controller/controller.js");
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/users", getUsers)
+app.get("/api/articles", getArticles)
 
 app.patch("/api/articles/:article_id", patchArticleById)
 
