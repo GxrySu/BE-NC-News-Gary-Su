41c0b2bd1a14f3fb9a5bfe70131fa9d08614e867
gxrysu <garysu2013@gmail.com>
2022-10-13 16:02:22 +0100

    Refactor 4 - Linking Heruko

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,21 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+require("dotenv").config({
+	path: `${__dirname}/../.env.${ENV}`,
 });
 
-const config =
-  ENV === 'production'
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+	throw new Error("PGDATABASE not set or DATABASE_URL not set");
 }
+const config =
+	ENV === "production"
+		? {
+				connectionString: process.env.DATABASE_URL,
+				ssl: {
+					rejectUnauthorized: false,
+				},
+		  }
+		: {};
 
 module.exports = new Pool(config);
